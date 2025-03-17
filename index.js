1042c91c28524ae773f32ac137c78fe6a0b8d6d7
viperr <Antonostr2004@gmail.com>
2025-03-17 21:02:54 +0300

    message

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict'
+// fetch('https://mystormnet.site/api/get_products_list/')
+// .then((res) => res.json())
+// .then((data) => {
+
+// })
+const img = document.querySelectorAll('.img')
+for(let app of img) {
+    app.addEventListener('click', function (event) {
+        
+    })
+}
