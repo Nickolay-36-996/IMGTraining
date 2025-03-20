f93232e809dd84b90f658bfcef1ac669905343a0
viperr <Antonostr2004@gmail.com>
2025-03-20 20:58:58 +0300

    message

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
-'use strict'
-// fetch('https://mystormnet.site/api/get_products_list/')
-// .then((res) => res.json())
-// .then((data) => {
-
-// })
-const img = document.querySelectorAll('.img')
-for(let app of img) {
-    app.addEventListener('click', function (event) {
-        
+"use strict";
+fetch('https://mystormnet.site/api/get_products_list/')
+.then((res) => res.json())
+.then((data) => {
+    const filters = data.results.filter((element, index) => {
+        if(index < 5) {
+            return element
+        }
     })
-}
+
+    const imgMain = document.querySelector('.img__main')
+    imgMain.setAttribute('src', filters[0].image_prev)
+   
+    for(let l of filters) {
+        const tag = document.createElement('img')
+        const path = l.image_prev
+        const imgAdd = document.querySelector('.img__add')
+        tag.setAttribute('src', path)
+        tag.classList.add('img')
+        imgAdd.append(tag)
+        tag.addEventListener('click', function (event) {
+            imgMain.setAttribute('src', tag.getAttribute('src'))
+        })
+    }
+})
+
+
+// const img = document.querySelectorAll(".img");
+// const imgMain = document.querySelector(".img__main");
+// for (let app of img) {
+//   app.addEventListener("click", function (event) {
+//     const img = event.target;
+//     img.getAttribute("src");
+//     imgMain.setAttribute("src", img.getAttribute('src'));
+//   });
+// }
